d7c6d04b93f7037f8f2f8513a48828e5917fc44f
geraldglynn <geraldglynn@gmail.com>
2021-02-07 17:30:43 +0000

    Removed unused properties from api responses

--- src/api/themoviedb.js
+++ src/api/themoviedb.js
@@ -1,3 +1,4 @@
+import pick from 'lodash/pick'
 const url = 'https://api.themoviedb.org/3'
 const apiKey = '291d7cc5431b3057427d4f7814163f9e'
 const language = 'en-US'
@@ -6,9 +7,21 @@ const language = 'en-US'
 export const fetchMovies = () => {
     return fetch(`${url}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&language${language}`)
     .then(res => res.json())
+    .then(json => json.results)
 }
 
 export const fetchMovie = (id) => {
   return fetch(`${url}/movie/${id}?sort_by=popularity.desc&api_key=${apiKey}&language${language}`)
   .then(res => res.json())
+  .then(json => pick(json,
+    [
+      'overview',
+      'release_date',
+      'vote_average',
+      'original_language',
+      'genres',
+      'production_companies',
+      'poster_path',
+    ]
+  ))
 }
--- src/components/list/index.jsx
+++ src/components/list/index.jsx
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from 'react'
+import pick from 'lodash/pick'
+
 import { fetchMovies } from '../../api/themoviedb'
+
 import Container from 'react-bootstrap/Container'
 import Item from '../item'
 
-function List(props) {
+function List() {
   const [ list, setList ] = useState([])
 
   useEffect(() => {
-    fetchMovies().then(response => setList(response.results))
-  }, [])
+    fetchMovies().then(response => setList(
+      response.map( item =>
+        pick(item,
+          [
+            'id',
+            'title',
+            'original_title',
+            'release_date',
+            'vote_average',
+          ]
+        ))
+      ))
+    }, [])
 
   return(
     <div>
@@ -19,7 +33,6 @@ function List(props) {
             key={item.id}
             id={item.id}
             title={item.title}
-            originalLanguage={item.original_language}
             originalTitle={item.original_title}
             releaseDate={item.release_date}
             voteAverage={item.vote_average}
