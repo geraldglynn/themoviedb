10c69063ec636f27410761c3ed7bcd00b1a610cb
geraldglynn <geraldglynn@gmail.com>
2021-02-07 19:29:33 +0000

    Added unit test for utils/movie genreString()

---
+++ src/utils/__tests__/movie.test.js
@@ -0,0 +1,26 @@
+import { genreString } from '../movie'
+
+describe('movie', () => {
+  describe('genreString', () => {
+
+    const genres = [
+      {
+        id: '1',
+        name: 'Horror',
+      },
+      {
+        id: '2',
+        name: 'Thriller',
+      },
+      {
+        id: '3',
+        name: 'Action',
+      },
+    ]
+
+    it('should return string seperated by ", "', ()=> {
+      expect(genreString(genres)).toBe('Horror, Thriller, Action')
+    })
+
+  })
+})
