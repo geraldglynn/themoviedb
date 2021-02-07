3b7a41f837eb83c04fd62811a58c9989988731b7
geraldglynn <geraldglynn@gmail.com>
2021-02-07 19:28:13 +0000

    Improved App.test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/showtime analytics/i);
+  const linkElement = screen.getByText('Showtime Analytics');
   expect(linkElement).toBeInTheDocument();
 });
